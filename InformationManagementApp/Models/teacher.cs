505aeda4cb3262fb09782470c89d37d18e3ff5fc
ishahruk96 <ishahruk96@gmail.com>
2019-10-25 12:48:21 +0600

    hub2

---
+++ InformationManagementApp/Models/teacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InformationManagementApp.Models
+{
+    public class teacher
+    {
+
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+        public int TeacherAge { get; set; }
+        public string TeacherRegNo { get; set; }
+        public string TeacherMobileNo { get; set; }
+        public string TeacherEmail { get; set; }
+        public string TeacherAddress { get; set; }
+
+        public teacher(string teacherName, int teacherAge, string teacherRegNo, string teacherMobileNo, string teacherEmail, string teacherAddress)
+        {
+            eacherName = tacherName;
+            tacherAge = tacherAge;
+            tacherRegNo = tacherRegNo;
+            tacherMobileNo = tacherMobileNo;
+            tacherEmail = tacherEmail;
+            tacherAddress = tacherAddress;
+        }
+    }
+}
